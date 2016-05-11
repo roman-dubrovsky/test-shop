a853098570312adf8b3c71694ae014f33b8a4024
UladzislauCharneny <chernyony2015@gmail.com>
2016-05-11 16:17:52 +0300

    Add validation for numbers

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,7 +7,7 @@ class Order < ActiveRecord::Base
 
   has_many :items, dependent: :destroy
 
-  validates_presence_of :user, :state, :phone, :address, :name
+  validates_presence_of :state, :phone, :address, :name
 
   def total_price
     items.map(&:price).sum
